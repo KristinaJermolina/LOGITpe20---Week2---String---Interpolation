9581007c2d7daa482b65359a4054341da9938fc7
Opilane TTHK <opilane@tthk.ee>
2020-09-08 12:15:26 +0300

    first commit

---
+++ StringInter/Program.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+
+namespace StringInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             1. The app asks for the user´s first name and stores it in a separate variable
+            2. The app asks for the user´s last name and stores it in a separate variable
+           */
+            string FirstName;
+            string LastName;
+            Console.WriteLine("What is your first name?");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            LastName = Console.ReadLine();
+
+            // Console.WriteLine("Hello, " + FirstName + " " + LastName);
+            //string interpolation
+            //$ - alt + cntrl+4 { -alt gr + 7, } - alt gr + 0
+            Console.WriteLine($"Hello, {FirstName} {LastName} ");
+
+            
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             1. Program asks for user´s first and lastname
+            2. Program asks for user´s year of birth
+            3. Program greets the user by their name and displays their age
+
+            NB! To display the result use one WriteLine line only!+
+             */
+
+            String FirstName;
+            String LastName;
+            Console.WriteLine("What is your first name?");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            LastName = Console.ReadLine();
+            Console.WriteLine("What is your birth of year?");
+            string UserInput;
+            UserInput = Console.ReadLine();
+            int Age;
+            Age = Int32.Parse(UserInput);
+            int Year = 2020 - Age;
+            Console.WriteLine("You were born in" + Age);
+            Console.WriteLine($"Hello, {FirstName} {LastName} {Age}");
+
+
+
+        }
+    }
+}
